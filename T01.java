81bddbc4f171bf88fdb88432290a5833fd907d49
JoiceNapitupulu <anastasyajoicen@gmail.com>
2024-09-04 21:27:12 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
+// 12S24020 - Joice Anastasya Napitupulu
 // NIM - NAMA
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+
+        iSBN = input.nextLine();
+        System.out.println(iSBN);
+    }
+}
